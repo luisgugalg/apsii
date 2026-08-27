a347062059fd8a41ca682430afe5de7fc08f71bc
Luís Gustavo <141464049+luisgugalg@users.noreply.github.com>
2026-08-26 21:50:41 -0300

    Update Estagiario.java

--- Exerciciotres/Estagiario.java
+++ Exerciciotres/Estagiario.java
@@ -9,12 +9,12 @@ public class Estagiario extends Funcionario {
         this.cargaHorariaSemanal = cargaHorariaSemanal;
     }
 
-    @Override
+    
     public double calcularSalario() {
         return valorBolsaAuxilio;
     }
 
-    @Override
+    
     public double calcularDesconto() {
         return 0.00;
     }
